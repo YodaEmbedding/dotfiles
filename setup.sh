6cdbb174e3c8225dfbe6caa75015f2bc0dd5718f
Mateen Ulhaq <mulhaq2005@gmail.com>
2017-10-01 01:50:39 -0700

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+stow -R bash
+stow -R colors
+stow -R compton
+stow -R dunst
+stow -R git
+stow -R i3
+stow -R i3blocks
+stow -R mimeapps
+stow -R mpv
+stow -R PC-Mordor-Arch -t /
+stow -R root -t /
+stow -R termite
+stow -R vim
+stow -R xinit
+stow -R xmodmap
+stow -R zsh
