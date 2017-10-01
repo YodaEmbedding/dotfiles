41586be042a058776f605132866413e80712b3d6
Mateen Ulhaq <mulhaq2005@gmail.com>
2017-10-01 01:08:54 -0700

    Added /usr/local scripts

---
+++ root/usr/local/share/pythonstartup.py
@@ -0,0 +1,12 @@
+import functools
+import itertools
+import math
+import os
+import sys
+from time import time
+
+import cv2
+import numpy as np
+
+from math import exp, log, sin, cos, tan, asin, acos, atan, atan2
+
