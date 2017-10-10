574e1aa63aa6c5c282fd1cfe4e2ce9ee06d34c64
Mateen Ulhaq <mulhaq2005@gmail.com>
2017-10-09 19:45:25 -0700

    Added sqrt to default python shell imports

--- root/usr/local/share/pythonstartup.py
+++ root/usr/local/share/pythonstartup.py
@@ -3,10 +3,11 @@ import itertools
 import math
 import os
 import sys
+
 from time import time
+from timeit import timeit
+from math import exp, log, sqrt, sin, cos, tan, asin, acos, atan, atan2
 
 import cv2
 import numpy as np
-
-from math import exp, log, sin, cos, tan, asin, acos, atan, atan2
 
