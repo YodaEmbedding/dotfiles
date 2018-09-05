a852eaf342d4ec5b3413a2d9d3c23a6d79bcaf67
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-09-05 16:39:09 -0700

    Added quick setup scripts for work computer

---
+++ ln_common.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $HOME/dotfiles/vim/.vimrc         $HOME/.vimrc
+ln -s $HOME/dotfiles/zsh/.zshrc         $HOME/.zshrc
+ln -s $HOME/dotfiles/xmodmap/.Xmodmap   $HOME/.Xmodmap
+ln -s $HOME/dotfiles/colors/.Xresources $HOME/.Xresources
+ln -s $HOME/dotfiles/colors/.dircolors  $HOME/.dircolors
---
+++ setup_quick_n_dirty.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# SYMLINK
+
+./ln_common.sh
+
+# ZSH
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+git clone https://github.com/zplug/zplug $HOME/.zplug
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
