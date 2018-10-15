aa0ba3013b25f4b0dd9e878ed919617c0fa03ffa
Mateen Ulhaq <mulhaq2005@gmail.com>
2018-10-14 23:16:38 -0700

    Added tmux

--- ln_common.sh
+++ ln_common.sh
@@ -3,6 +3,7 @@
 ln -s $HOME/dotfiles/bash/.aliases      $HOME/.aliases
 ln -s $HOME/dotfiles/colors/.Xresources $HOME/.Xresources
 ln -s $HOME/dotfiles/colors/.dircolors  $HOME/.dircolors
+ln -s $HOME/dotfiles/tmux/.tmux.conf    $HOME/.tmux.conf
 ln -s $HOME/dotfiles/vim/.vimrc         $HOME/.vimrc
 ln -s $HOME/dotfiles/xmodmap/.Xmodmap   $HOME/.Xmodmap
 ln -s $HOME/dotfiles/zsh/.zshrc         $HOME/.zshrc
