62e388956a9eb8c811bcd32f4db9d371955a54f3
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-06-27 04:38:01 -0700

    Statusbar tweaks

--- polybar/.config/polybar/polybar.sh
+++ polybar/.config/polybar/polybar.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
 killall -q polybar
-
-while pgrep -x polybar >/dev/null; do sleep 0.1; done
-
-#polybar traybar -c ~/.config/polybar/config &
-#polybar topbar1 -c ~/.config/polybar/config &
-#polybar topbar2 -c ~/.config/polybar/config &
-
+while pgrep -x polybar >/dev/null; do sleep 0.01; done
 polybar top -c ~/.config/polybar/config &
-
-
