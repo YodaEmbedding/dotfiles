66ef772da4c961a87c9c078d83c2ad26ed4a8efb
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-03-07 20:34:43 -0800

    root(pythonstartup): lazy loadable modules/methods/classes

--- root/usr/local/share/pythonstartup.py
+++ root/usr/local/share/pythonstartup.py
@@ -6,6 +6,7 @@ import os
 import re
 import sys
 from functools import partial
+from importlib import import_module
 from itertools import *
 from pprint import pprint
 from time import time, sleep
@@ -19,3 +20,34 @@ from numpy import pi, sin, cos, tan, arcsin, arccos, arctan, arctan2, degrees, r
 np.set_printoptions(precision=3)
 
 j = 1.j
+
+
+class LazyModule:
+    def __init__(self, alias, path):
+        self._alias = alias
+        self._path = path
+        globals()[self._alias] = self
+
+    def __getattr__(self, attr):
+        module = import_module(self._path)
+        globals()[self._alias] = module
+        return getattr(module, attr)
+
+
+def lazy_callable(alias, path):
+    def inner(*args, **kwargs):
+        module = import_module(path)
+        func = getattr(module, alias)
+        globals()[alias] = func
+        return func(*args, **kwargs)
+
+    globals()[alias] = inner
+
+
+LazyModule("mpl", "matplotlib")
+LazyModule("plt", "matplotlib.pyplot")
+LazyModule("pd", "pandas")
+LazyModule("sns", "seaborn")
+LazyModule("tf", "tensorflow")
+
+lazy_callable("DataFrame", "pandas")
