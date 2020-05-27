7518da7dbc806337abb81a6e8cc02f42e83ce14e
Mateen Ulhaq <mulhaq2005@gmail.com>
2020-05-27 14:15:17 -0700

    misc: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,29 @@
+stow -R PC-Mordor-Arch -t /
+stow -R root -t /
+
+stow -R alacritty
 stow -R bash
 stow -R colors
+stow -R devilspie
 stow -R dunst
 stow -R git
 stow -R i3
 stow -R i3blocks
+stow -R latexmk
+stow -R lint
 stow -R mimeapps
 stow -R mpv
-stow -R PC-Mordor-Arch -t /
+stow -R ncmpcpp
 stow -R picom
-stow -R root -t /
+stow -R polybar
+stow -R ranger
+stow -R rofi
+stow -R scripts
+stow -R systemd
 stow -R termite
+stow -R tmux
 stow -R vim
 stow -R xinit
 stow -R xmodmap
+stow -R zathura
 stow -R zsh
