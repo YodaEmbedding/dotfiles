d90effce67da31732ef83c10da71c8f0ba291d34
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-01-16 20:50:55 -0800

    nvim(lua/options): implement workarounds

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,38 +1,54 @@
-local opt = vim.api.nvim_set_option
+local home = os.getenv("HOME")
+local g = vim.g
+
+-- Workaround until https://github.com/neovim/neovim/pull/13479
+local function o(key, value)
+  vim.o[key] = value
+end
+
+local function b(key, value)
+  vim.bo[key] = value
+  vim.o[key] = value
+end
+
+local function w(key, value)
+  vim.wo[key] = value
+  vim.o[key] = value
+end
 
 -- Indenting, tabs, spacing
-opt("expandtab", true)              -- Spaces for indent
-opt("joinspaces", false)            -- Join sentences with single space
-opt("shiftround", true)             -- Round >> to nearest shiftwidth multiple
-opt("shiftwidth", 4)                -- Spaces for >>
-opt("softtabstop", 4)               -- Spaces for tab
-opt("tabstop", 4)                   -- Tab display width
+b("expandtab", true)                  -- Spaces for indent
+o("joinspaces", false)                -- Join sentences with single space
+o("shiftround", true)                 -- Round >> to nearest shiftwidth multiple
+b("shiftwidth", 4)                    -- Spaces for >>
+b("softtabstop", 4)                   -- Spaces for tab
+b("tabstop", 4)                       -- Tab display width
 
 -- Indenting (python)
-vim.g.pyindent_continue     = '&sw'
-vim.g.pyindent_nested_paren = '&sw'
-vim.g.pyindent_open_paren   = '&sw'
+g.pyindent_continue     = "&sw"
+g.pyindent_nested_paren = "&sw"
+g.pyindent_open_paren   = "&sw"
 
 -- Miscellaneous
-opt("hidden", true)                 -- Allow hiding unsaved buffers
-opt("wildmenu", true)               -- Enhanced command-line completion
-opt("wildmode", "longest,list,full")
+o("hidden", true)                     -- Allow hiding unsaved buffers
+o("mouse", "a")                       -- Mouse interactivity
+o("wildmenu", true)                   -- Enhanced command-line completion
+o("wildmode", "longest,list,full")    -- Command-line completion behavior
 
 -- Search
-opt("hlsearch", true)               -- Highlight search results
-opt("ignorecase", true)             -- Ignore case
-opt("inccommand", "split")          -- Incremental highlight during :%s
-opt("smartcase", true)              -- Ignore case if lower
+o("hlsearch", true)                   -- Highlight search results
+o("ignorecase", true)                 -- Ignore case
+o("inccommand", "split")              -- Incremental highlight during :%s
+o("smartcase", true)                  -- Ignore case if lower
 
 -- Undo
-opt("undodir", "$HOME/.vim/undo")   -- Undo history location
-opt("undofile", true)               -- Undo history
+o("undodir", home.."/.vim/undo")      -- Undo history location
+b("undofile", true)                   -- Undo history
 
 -- Visual
-opt("colorcolumn", "80")            -- Column guide
-opt("cursorline", true)             -- Highlight current line
-opt("laststatus", 2)                -- Status line
-opt("mouse", "a")                   -- Mouse interactivity
-opt("numberwidth", 2)               -- Numbering column width
-opt("relativenumber", true)         -- Relative line numbers
-opt("scrolloff", 2)                 -- Always display lines around cursor
+w("colorcolumn", "80")                -- Column guide
+w("cursorline", true)                 -- Highlight current line
+o("laststatus", 2)                    -- Status line
+w("numberwidth", 2)                   -- Numbering column width
+w("relativenumber", true)             -- Relative line numbers
+w("scrolloff", 2)                     -- Always display lines around cursor
