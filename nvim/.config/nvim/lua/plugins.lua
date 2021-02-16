7dcb080c3195a9433ced6d77d7c676e25cee2c64
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-02-16 05:21:56 -0800

    nvim(lua/plugins): auto-PackerCompile on plugins.lua write

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,4 +1,5 @@
 vim.cmd [[packadd packer.nvim]]
+vim.cmd [[autocmd BufWritePost plugins.lua PackerCompile]]
 
 return require("packer").startup(function()
   use {"wbthomason/"        .. "packer.nvim", opt = true} -- Plugin manager
