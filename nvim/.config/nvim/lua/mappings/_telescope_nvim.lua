f2b9258458f9bf6fd59eeedd4583ea81558499b5
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-06-25 03:20:37 -0700

    nvim(lua/mappings): telescope.nvim < instead of <space> when reasonable

--- nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
@@ -7,11 +7,11 @@ local vimp = require("vimp")
 
 local nnoremap = vimp.nnoremap
 
-nnoremap(",",               b.git_files)
-nnoremap("\\f",             b.find_files)
-nnoremap("<Tab>",           function () b.buffers {sort_lastused=true} end)
 nnoremap("<Space><Space>",  b.builtin)
-nnoremap("<Space>g",        b.live_grep)
-nnoremap("<Space>m",        b.marks)
-nnoremap("<Space>h",        b.command_history)
+nnoremap("<Tab>",           function () b.buffers {sort_lastused=true} end)
+nnoremap(",",               b.git_files)
+nnoremap("<f",              b.find_files)
+nnoremap("<l",              b.live_grep)
+nnoremap("<m",              b.marks)
+nnoremap("<:",              b.command_history)
 nnoremap("gr",              b.lsp_references)
