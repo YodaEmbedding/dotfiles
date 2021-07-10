94e34a8e83466cd91e9ce470472f5065a8b2af1b
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-07-10 02:47:18 -0700

    nvim(lua/plugins): lightspeed.nvim

---
+++ nvim/.config/nvim/lua/mappings/_lightspeed_nvim.lua
@@ -0,0 +1,20 @@
+if not _G.plugin_loaded("lightspeed.nvim") then
+  do return end
+end
+
+
+local lightspeed = require("lightspeed")
+local vimp = require("vimp")
+
+local nnoremap = vimp.nnoremap
+local xnoremap = vimp.xnoremap
+
+local function repeat_ft(reverse)
+  lightspeed.ft["instant-repeat?"] = true
+  lightspeed.ft:to(reverse, lightspeed.ft["prev-t-like?"])
+end
+
+nnoremap({"silent"}, ";", function () repeat_ft(false) end)
+xnoremap({"silent"}, ";", function () repeat_ft(false) end)
+-- nnoremap({"silent"}, ",", function () repeat_ft(true) end)
+xnoremap({"silent"}, ",", function () repeat_ft(true) end)
---
+++ nvim/.config/nvim/lua/plugins/_lightspeed_nvim.lua
@@ -0,0 +1,7 @@
+if not _G.plugin_loaded("lightspeed.nvim") then
+  do return end
+end
+
+require("lightspeed").setup {
+
+}
