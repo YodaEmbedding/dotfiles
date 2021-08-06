2371badec02e74552ff35cfb1a279728cb47c81e
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-08-06 01:28:45 -0700

    nvim(lua/plugins): lsp_signature.nvim width

--- nvim/.config/nvim/lua/plugins/_lsp_signature_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_lsp_signature_nvim.lua
@@ -2,4 +2,7 @@ if not _G.plugin_loaded("lsp_signature.nvim") then
   do return end
 end
 
-require("lsp_signature").on_attach()
+require("lsp_signature").on_attach {
+--fix_pos = true,
+  max_width = 80,
+}
