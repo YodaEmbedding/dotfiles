75dfb45a2281115386ec6555cd938d0b92d5bc5a
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-08-06 01:37:47 -0700

    nvim(lua/plugins): enable treesitter

--- nvim/.config/nvim/lua/plugins/_treesitter.lua
+++ nvim/.config/nvim/lua/plugins/_treesitter.lua
@@ -4,9 +4,9 @@ end
 
 require("nvim-treesitter.configs").setup {
   ensure_installed = "all",
-  -- highlight = {
-  --   enable = true,
-  -- },
+  highlight = {
+    enable = true,
+  },
   incremental_selection = {
     enable = true,
     keymaps = {
