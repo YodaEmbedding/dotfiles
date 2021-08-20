10469ddac84d2eda8c809163121bcd5bafc99221
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-08-19 19:33:58 -0700

    nvim(lua/plugins): refactor

--- nvim/.config/nvim/lua/plugins/lspconfig/diagnosticls.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig/diagnosticls.lua
@@ -1,56 +1,59 @@
-local function setup(args)
-  require("lspconfig").diagnosticls.setup {
-    on_attach = args.on_attach,
+local nvim_lsp = require("lspconfig")
+
+local config = {
+  filetypes = {
+    "css",
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+  },
+  init_options = {
     filetypes = {
-      "css",
-      "javascript",
-      "javascriptreact",
-      "typescript",
-      "typescriptreact",
+      javascript = "eslint",
+      typescript = "eslint",
+      javascriptreact = "eslint",
+      typescriptreact = "eslint",
     },
-    init_options = {
-      filetypes = {
-        javascript = "eslint",
-        typescript = "eslint",
-        javascriptreact = "eslint",
-        typescriptreact = "eslint",
-      },
-      linters = {
-        eslint = {
-          sourceName = "eslint",
-          command = "./node_modules/.bin/eslint",
-          rootPatterns = {
-            ".eslintrc.js",
-            "package.json",
-            ".git",
-          },
-          debounce = 100,
-          args = {
-            "--cache",
-            "--stdin",
-            "--stdin-filename",
-            "%filepath",
-            "--format",
-            "json",
-          },
-          parseJson = {
-            errorsRoot = "[0].messages",
-            line = "line",
-            column = "column",
-            endLine = "endLine",
-            endColumn = "endColumn",
-            message = "${message} [${ruleId}]",
-            security = "severity",
-          },
-          securities = {
-            [2] = "error",
-            [1] = "warning",
-          },
+    linters = {
+      eslint = {
+        sourceName = "eslint",
+        command = "./node_modules/.bin/eslint",
+        rootPatterns = {
+          ".eslintrc.js",
+          "package.json",
+          ".git",
+        },
+        debounce = 100,
+        args = {
+          "--cache",
+          "--stdin",
+          "--stdin-filename",
+          "%filepath",
+          "--format",
+          "json",
+        },
+        parseJson = {
+          errorsRoot = "[0].messages",
+          line = "line",
+          column = "column",
+          endLine = "endLine",
+          endColumn = "endColumn",
+          message = "${message} [${ruleId}]",
+          security = "severity",
+        },
+        securities = {
+          [2] = "error",
+          [1] = "warning",
         },
       },
     },
-  }
+  },
+}
+
+local function setup(args)
+  config.on_attach = args.on_attach
+  nvim_lsp.diagnosticls.setup(config)
 end
 
 return setup
-
