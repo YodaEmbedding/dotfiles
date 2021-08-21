890af074832e143f9a3886697387a88a3beef3aa
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-08-20 20:31:15 -0700

    nvim(lua/plugins): coq auto_start instead of vim.schedule :COQnow

--- nvim/.config/nvim/lua/plugins/_coq_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_coq_nvim.lua
@@ -3,12 +3,10 @@ if not _G.plugin_loaded("coq_nvim") then
 end
 
 vim.g.coq_settings = {
-  -- auto_start = true,
+  auto_start = "shut-up",
   ["display.preview.x_max_len"] = 40,
   ["keymap.recommended"] = false,
   ["weights.recency"] = 0.5,
 }
 
-vim.schedule(function()
-  vim.cmd [[COQnow -s]]
-end)
+require("coq")
