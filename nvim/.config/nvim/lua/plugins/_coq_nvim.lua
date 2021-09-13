c525757a2de0d3d05a610a00f9022a3f4e81b550
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-09-13 05:03:01 -0700

    nvim(lua/plugins): coq keymap.repeat

--- nvim/.config/nvim/lua/plugins/_coq_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_coq_nvim.lua
@@ -6,6 +6,7 @@ vim.g.coq_settings = {
   auto_start = "shut-up",
   ["display.preview.x_max_len"] = 40,
   ["keymap.recommended"] = false,
+  ["keymap.repeat"] = "<Space>.",
   ["weights.recency"] = 0.5,
 }
 
