472cdb6037cd8718af3c0131555aba58166450ca
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-02-10 20:12:25 -0800

    nvim(plugins): indent-blankline lua conversion

--- nvim/.config/nvim/lua/plugins/_indent_blankline_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_indent_blankline_nvim.lua
@@ -2,6 +2,7 @@ if not _G.plugin_loaded("indent-blankline.nvim") then
   do return end
 end
 
-vim.cmd [[let g:indentLine_char = '│']]
-vim.cmd [[let g:indent_blankline_show_first_indent_level = v:false]]
--- vim.cmd [[let g:indent_blankline_use_treesitter = v:true]]
+require("indent_blankline").setup {
+  char = "│",
+  show_first_indent_level = false,
+}
