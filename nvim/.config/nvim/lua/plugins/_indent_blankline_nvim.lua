982abd72fbe00b8b1a5034f2fd001b0fe622ac1c
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-02-10 20:25:10 -0800

    nvim(plugins): indent-blankline context highlighting

--- nvim/.config/nvim/lua/plugins/_indent_blankline_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_indent_blankline_nvim.lua
@@ -4,5 +4,8 @@ end
 
 require("indent_blankline").setup {
   char = "│",
+  show_current_context = true,
+  show_current_context_start = true,
   show_first_indent_level = false,
+  use_treesitter = true,
 }
