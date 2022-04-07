c4e83e42c9b43e1590b13d395815541c6e4be307
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-04-06 18:26:32 -0700

    nvim(plugins): cmp: upgrade config

--- nvim/.config/nvim/lua/mappings/_nvim_cmp.lua
+++ nvim/.config/nvim/lua/mappings/_nvim_cmp.lua
@@ -3,13 +3,13 @@ local cmp = require("cmp")
 return {
   ["<Tab>"] = cmp.mapping.select_next_item(),
   ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
   ["<C-e>"] = cmp.mapping.confirm({
     behavior = cmp.ConfirmBehavior.Insert,
     select = true,
   }),
-  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-  ["<C-f>"] = cmp.mapping.scroll_docs(4),
+  ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+  ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
   ["<PageUp>"] = function(fallback)
     for i = 1, 8 do
       cmp.mapping.select_prev_item()(nil)
