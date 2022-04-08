d39878bd92ee325ec844f88714bfb28f710ad6a0
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-04-08 04:08:09 -0700

    setup: gnu -> musl (seems to not depend on specific versions of GLIBC

--- setup/installers/delta.sh
+++ setup/installers/delta.sh
@@ -5,8 +5,8 @@ mkdir -p "$TMP_DIR"
 cd "$TMP_DIR" || exit 1
 
 DELTA_VERSION=0.12.1
-wget -nc "https://github.com/dandavison/delta/releases/download/$DELTA_VERSION/delta-$DELTA_VERSION-x86_64-unknown-linux-gnu.tar.gz"
-tar xf "delta-$DELTA_VERSION-x86_64-unknown-linux-gnu.tar.gz"
-cd "delta-$DELTA_VERSION-x86_64-unknown-linux-gnu" || exit 1
+wget -nc "https://github.com/dandavison/delta/releases/download/$DELTA_VERSION/delta-$DELTA_VERSION-x86_64-unknown-linux-musl.tar.gz"
+tar xf "delta-$DELTA_VERSION-x86_64-unknown-linux-musl.tar.gz"
+cd "delta-$DELTA_VERSION-x86_64-unknown-linux-musl" || exit 1
 mkdir -p "$HOME/.local/bin/"
 cp "delta" "$HOME/.local/bin/"
