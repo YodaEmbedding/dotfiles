4f48ea02fbcb57f228c7f6cab417b4ea54cbadbf
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-04-08 04:04:16 -0700

    setup: fix: installers/delta

--- setup/installers/delta.sh
+++ setup/installers/delta.sh
@@ -4,8 +4,7 @@ TMP_DIR="$HOME/.dotfiles_tmp_install"
 mkdir -p "$TMP_DIR"
 cd "$TMP_DIR" || exit 1
 
-DELTA_VERSION=0.20.0
-https://github.com/dandavison/delta/releases/download/0.12.1/delta-0.12.1-x86_64-unknown-linux-gnu.tar.gz
+DELTA_VERSION=0.12.1
 wget -nc "https://github.com/dandavison/delta/releases/download/$DELTA_VERSION/delta-$DELTA_VERSION-x86_64-unknown-linux-gnu.tar.gz"
 tar xf "delta-$DELTA_VERSION-x86_64-unknown-linux-gnu.tar.gz"
 cd "delta-$DELTA_VERSION-x86_64-unknown-linux-gnu" || exit 1
