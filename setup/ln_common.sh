0f202246556ebfb6bc16859e035205a192ebd761
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-04-08 02:13:23 -0700

    setup: remove ln_common.sh

--- setup/ln_common.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ln -s $HOME/dotfiles/bash/.aliases      $HOME/.aliases
-ln -s $HOME/dotfiles/colors/.Xresources $HOME/.Xresources
-ln -s $HOME/dotfiles/colors/.dircolors  $HOME/.dircolors
-ln -s $HOME/dotfiles/tmux/.tmux.conf    $HOME/.tmux.conf
-ln -s $HOME/dotfiles/vim/.vimrc         $HOME/.vimrc
-ln -s $HOME/dotfiles/xmodmap/.Xmodmap   $HOME/.Xmodmap
-ln -s $HOME/dotfiles/zsh/.zshrc         $HOME/.zshrc
