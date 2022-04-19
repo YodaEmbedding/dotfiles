2146bfef223bda3941c03a9a9fc0c6087889076b
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-04-18 22:01:25 -0700

    nvim(mappings): lightspeed.nvim disable default mappings by default

--- nvim/.config/nvim/lua/mappings/_lightspeed_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_lightspeed_nvim.lua
@@ -6,40 +6,62 @@ end
 local lightspeed = require("lightspeed")
 local vimp = require("vimp")
 
-local nnoremap = vimp.nnoremap
-local xnoremap = vimp.xnoremap
+local n = vimp.nmap
+local x = vimp.vmap
+local o = vimp.omap
 
 local function repeat_ft(reverse)
   lightspeed.ft["instant-repeat?"] = true
   lightspeed.ft:to(reverse, lightspeed.ft["prev-t-like?"])
 end
 
-nnoremap({"silent", "override"}, ";", function () repeat_ft(false) end)
-xnoremap({"silent", "override"}, ";", function () repeat_ft(false) end)
 
--- nnoremap({"silent", "override"}, ",", function () repeat_ft(true) end)
-xnoremap({"silent", "override"}, ",", function () repeat_ft(true) end)
+-- DEFAULT MAPPINGS
+
+n ("s", "<Plug>Lightspeed_s")
+n ("S", "<Plug>Lightspeed_S")
+x ("s", "<Plug>Lightspeed_s")
+x ("S", "<Plug>Lightspeed_S")
+o ("z", "<Plug>Lightspeed_s")
+o ("Z", "<Plug>Lightspeed_S")
+
+-- n ("gs", "<Plug>Lightspeed_gs")
+-- n ("gS", "<Plug>Lightspeed_gS")
+
+o ("x", "<Plug>Lightspeed_x")
+o ("X", "<Plug>Lightspeed_X")
+
+-- n ("f", "<Plug>Lightspeed_f")
+-- n ("F", "<Plug>Lightspeed_F")
+-- x ("f", "<Plug>Lightspeed_f")
+-- x ("F", "<Plug>Lightspeed_F")
+-- o ("f", "<Plug>Lightspeed_f")
+-- o ("F", "<Plug>Lightspeed_F")
+
+-- n ("t", "<Plug>Lightspeed_t")
+-- n ("T", "<Plug>Lightspeed_T")
+-- x ("t", "<Plug>Lightspeed_t")
+-- x ("T", "<Plug>Lightspeed_T")
+-- o ("t", "<Plug>Lightspeed_t")
+-- o ("T", "<Plug>Lightspeed_T")
+
+-- n (";", "<Plug>Lightspeed_;_ft")
+-- x (";", "<Plug>Lightspeed_;_ft")
+-- o (";", "<Plug>Lightspeed_;_ft")
+
+-- n (",", "<Plug>Lightspeed_,_ft")
+-- x (",", "<Plug>Lightspeed_,_ft")
+-- o (",", "<Plug>Lightspeed_,_ft")
 
 
--- DISABLE SOME DEFAULT MAPPINGS
+-- CUSTOM OVERRIDES
 
-nnoremap({"silent", "override"}, "gs", "gs")
-nnoremap({"silent", "override"}, "gS", "gS")
+-- n ("s", "<Plug>Lightspeed_omni_s")
+-- x ("s", "<Plug>Lightspeed_omni_s")
+-- o ("z", "<Plug>Lightspeed_omni_s")
 
-nnoremap({"silent", "override"}, "f", "f")
-xnoremap({"silent", "override"}, "f", "f")
-
-nnoremap({"silent", "override"}, "F", "F")
-xnoremap({"silent", "override"}, "F", "F")
-
-nnoremap({"silent", "override"}, "t", "t")
-xnoremap({"silent", "override"}, "t", "t")
-
-nnoremap({"silent", "override"}, "T", "T")
-xnoremap({"silent", "override"}, "T", "T")
-
-nnoremap({"silent", "override"}, ";", ";")
-xnoremap({"silent", "override"}, ";", ";")
-
--- nnoremap({"silent", "override"}, ",", ",")
-xnoremap({"silent", "override"}, ",", ",")
+-- n ({"silent", "override"}, ";", function () repeat_ft(false) end)
+-- x ({"silent", "override"}, ";", function () repeat_ft(false) end)
+--
+-- -- n ({"silent", "override"}, ",", function () repeat_ft(true) end)
+-- x ({"silent", "override"}, ",", function () repeat_ft(true) end)
--- nvim/.config/nvim/lua/plugins/_lightspeed_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_lightspeed_nvim.lua
@@ -2,6 +2,8 @@ if not _G.plugin_loaded("lightspeed.nvim") then
   do return end
 end
 
+vim.g.lightspeed_no_default_keymaps = true
+
 require("lightspeed").setup {
   ignore_case = true,
 }
