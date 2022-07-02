af2d4248108fdf58d8e6a857a364a2174c858278
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-07-02 15:35:28 -0700

    setup: setup_minimal_idcc_linux_node

---
+++ setup/setup_minimal_idcc_linux_node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+TMP_DIR="$HOME/.dotfiles_tmp_install"
+mkdir -p "$TMP_DIR"
+
+./setup/installers/m4.sh
+./setup/installers/autoconf.sh
+./setup/installers/automake.sh
+./setup/installers/gettext.sh
+./setup/installers/cmake.sh
+
+./setup/setup_minimal_linux.sh
