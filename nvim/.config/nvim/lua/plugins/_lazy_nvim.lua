a9b41975547039935a74acb05616be5572a67f3a
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-01-24 21:42:56 -0800

    nvim(lua/plugins): lazy.nvim on install, checkout tags/stable

--- nvim/.config/nvim/lua/plugins/_lazy_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_lazy_nvim.lua
@@ -2,16 +2,14 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
   })
+  vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" })
 end
 
 vim.opt.rtp:prepend(lazypath)
+
 
 ---@format disable-next
 local plugins = {
