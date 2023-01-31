6c57992db20f21bbf4dc86abd9d8fabda15fbff1
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-01-31 03:24:43 -0800

    nvim(lua/plugins): alpha.nvim header

--- nvim/.config/nvim/lua/plugins/_alpha_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_alpha_nvim.lua
@@ -1,3 +1,8 @@
-require("alpha").setup(
-  require("alpha.themes.startify").config
-)
+local alpha = require("alpha")
+local theme = require("alpha.themes.startify").config
+
+theme.layout[2].val = {
+  "1.e4 e5 2.Ke2 Ke7 3.Ke1 Ke8 4.Ke2 Ke7 5.Ke1 Ke8 6.Ke2 Ke7 1/2-1/2",
+}
+
+alpha.setup(theme)
