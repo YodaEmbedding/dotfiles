1ce8b3eb224e71e4d8e10640b29edf9f1d816670
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-03-03 02:05:18 -0800

    nvim(lua/plugins): nvim-lightbulb settings

---
+++ nvim/.config/nvim/lua/plugins/_nvim_lightbulb.lua
@@ -0,0 +1,6 @@
+require("nvim-lightbulb").setup {
+  autocmd = { enabled = true },
+  -- float = { enabled = true },
+  -- virtual_text = { enabled = true },
+  -- status_text = { enabled = true },
+}
