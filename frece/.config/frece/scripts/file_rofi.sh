6c2ab18dceaca8412e710b81a1d875b72d2adb51
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-05-02 18:29:59 -0700

    frece: ROFI path

--- frece/.config/frece/scripts/file_rofi.sh
+++ frece/.config/frece/scripts/file_rofi.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+ROFI="${ROFI:-rofi}"
 DATA_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/frece"
 DB_FILE="$DATA_DIR/file.db"
 
-item=$(frece print "$DB_FILE" | rofi "$@" -dmenu)
+item=$(frece print "$DB_FILE" | "$ROFI" "$@" -dmenu)
 [[ -z $item ]] && exit 1
 frece increment "$DB_FILE" "$item"
 
