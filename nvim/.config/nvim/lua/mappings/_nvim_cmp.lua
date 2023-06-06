e67ec5e8ed83e9a3095b3dc59af5f940caf5de40
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-06-05 18:12:33 -0700

    nvim(lua/mappings): nvim-cmp: confirm (refactor)

--- nvim/.config/nvim/lua/mappings/_nvim_cmp.lua
+++ nvim/.config/nvim/lua/mappings/_nvim_cmp.lua
@@ -1,15 +1,32 @@
 local cmp = require("cmp")
 
+local confirm = cmp.mapping.confirm({
+  behavior = cmp.ConfirmBehavior.Replace,
+  select = true,
+})
+
+local function __confirm(fallback)
+  local ok, copilot_suggestion = pcall(require, "copilot.suggestion")
+  if ok and copilot_suggestion.is_visible() then
+    copilot_suggestion.accept()
+  elseif cmp.visible() then
+    cmp.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    })
+    -- cmp.complete()
+  else
+    fallback()
+  end
+end
+
 return {
   ["<C-n>"] = cmp.mapping.select_next_item(),
   ["<C-p>"] = cmp.mapping.select_prev_item(),
   ["<Tab>"] = cmp.mapping.select_next_item(),
   ["<S-Tab>"] = cmp.mapping.select_prev_item(),
   ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-  ["<C-e>"] = cmp.mapping.confirm({
-    behavior = cmp.ConfirmBehavior.Replace,
-    select = true,
-  }),
+  ["<C-e>"] = confirm,
   ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
   ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
   ["<PageUp>"] = function(fallback)
