d21b9d515323d58913241197f6260cbd4c48e9bd
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-06-05 21:52:46 -0700

    nvim(lua/plugins): copilot.lua: mappings

--- nvim/.config/nvim/lua/plugins/_copilot_lua.lua
+++ nvim/.config/nvim/lua/plugins/_copilot_lua.lua
@@ -22,16 +22,16 @@ return {
       },
 
       suggestion = {
-        enabled = false,       -- default: true
-        auto_trigger = false,  -- default: false
-        debounce = 75,         -- default: 75
+        enabled = false,         -- default: true
+        auto_trigger = false,    -- default: false
+        debounce = 75,           -- default: 75
         keymap = {
-          accept = "<M-l>",    -- default: "<M-l>"
-          accept_word = false, -- default: false
-          accept_line = false, -- default: false
-          next = "<M-]>",      -- default: "<M-]>"
-          prev = "<M-[>",      -- default: "<M-[>"
-          dismiss = "<C-]>",   -- default: "<C-]>"
+          accept      = "<C-t>", -- default: "<M-l>"
+          accept_word = "<C-w>", -- default: false
+          accept_line = "<C-l>", -- default: false
+          next        = "<C-f>", -- default: "<M-]>"
+          prev        = "<C-b>", -- default: "<M-[>"
+          dismiss     = "<C-g>", -- default: "<C-]>"
         },
       },
 
