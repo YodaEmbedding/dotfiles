ac416099f6c4cec4137ca984e90eeca23b1619af
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-04 23:55:45 -0700

    nvim(lua/options): rewrite using vim.opt

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,34 +1,19 @@
-local home = os.getenv("HOME")
 local g = vim.g
-
--- Workaround until https://github.com/neovim/neovim/pull/13479
-local function o(key, value)
-  vim.o[key] = value
-end
-
-local function b(key, value)
-  vim.bo[key] = value
-  vim.o[key] = value
-end
-
-local function w(key, value)
-  vim.wo[key] = value
-  vim.o[key] = value
-end
+local o = vim.opt
 
 ---@format disable
 
 -- Completion
-o("completeopt", "menuone,noinsert,noselect")
-o("shortmess", vim.o.shortmess .. "c")
+o.completeopt = "menuone,noinsert,noselect"
+vim.o.shortmess = vim.o.shortmess .. "c"
 
 -- Indenting, tabs, spacing
-b("expandtab", true)                  -- Spaces for indent
-o("joinspaces", false)                -- Join sentences with single space
-o("shiftround", true)                 -- Round >> to nearest shiftwidth multiple
-b("shiftwidth", 4)                    -- Spaces for >>
-b("softtabstop", 4)                   -- Spaces for tab
-b("tabstop", 4)                       -- Tab display width
+o.expandtab = true                    -- Spaces for indent
+o.joinspaces = false                  -- Join sentences with single space
+o.shiftround = true                   -- Round >> to nearest shiftwidth multiple
+o.shiftwidth = 4                      -- Spaces for >>
+o.softtabstop = 4                     -- Spaces for tab
+o.tabstop = 4                         -- Tab display width
 
 -- Indenting (python)
 g.pyindent_continue     = "&sw"
@@ -36,26 +21,26 @@ g.pyindent_nested_paren = "&sw"
 g.pyindent_open_paren   = "&sw"
 
 -- Miscellaneous
-o("hidden", true)                     -- Allow hiding unsaved buffers
-o("mouse", "a")                       -- Mouse interactivity
-o("wildmenu", true)                   -- Enhanced command-line completion
-o("wildmode", "longest,list,full")    -- Command-line completion behavior
-o("updatetime", 500)                  -- ms until CursorHold fires and swap save
+o.hidden = true                       -- Allow hiding unsaved buffers
+o.mouse = "a"                         -- Mouse interactivity
+o.wildmenu = true                     -- Enhanced command-line completion
+o.wildmode = "longest,list,full"      -- Command-line completion behavior
+o.updatetime = 500                    -- ms until CursorHold fires and swap save
 
 -- Search
-o("hlsearch", true)                   -- Highlight search results
-o("ignorecase", true)                 -- Ignore case
-o("inccommand", "split")              -- Incremental highlight during :%s
-o("smartcase", true)                  -- Ignore case if lower
+o.hlsearch = true                     -- Highlight search results
+o.ignorecase = true                   -- Ignore case
+o.inccommand = "split"                -- Incremental highlight during :%s
+o.smartcase = true                    -- Ignore case if lower
 
 -- Undo
-b("undofile", true)                   -- Undo history
+o.undofile = true                     -- Undo history
 
 -- Visual
-w("colorcolumn", "80")                -- Column guide
-w("cursorline", true)                 -- Highlight current line
-o("laststatus", 2)                    -- Status line
-w("linebreak", true)                  -- Wrap on word breaks
-w("numberwidth", 2)                   -- Numbering column width
-w("relativenumber", true)             -- Relative line numbers
-w("scrolloff", 2)                     -- Always display lines around cursor
+o.colorcolumn = "80"                  -- Column guide
+o.cursorline = true                   -- Highlight current line
+o.laststatus = 2                      -- Status line
+o.linebreak = true                    -- Wrap on word breaks
+o.numberwidth = 2                     -- Numbering column width
+o.relativenumber = true               -- Relative line numbers
+o.scrolloff = 2                       -- Always display lines around cursor
