b59d142c0d429edfe6cafa7aff6f4cacf82a283d
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-06 00:06:06 -0700

    nvim(lua/plugins): lsp_signature.nvim [enable]

--- nvim/.config/nvim/lua/plugins/_lsp_signature_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_lsp_signature_nvim.lua
@@ -1,8 +1,6 @@
 return {
   "ray-x/lsp_signature.nvim",
 
-  enabled = false,
-
   _ = {
     on_attach = function()
       if not _G.plugin_loaded("lsp_signature.nvim") then
@@ -12,6 +10,8 @@ return {
       require("lsp_signature").on_attach {
         --fix_pos = true,
         max_width = 80,
+        floating_window = false,
+        hint_enable = true,
       }
     end
   },
