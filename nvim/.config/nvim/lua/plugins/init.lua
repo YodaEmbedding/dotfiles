51994d163ae932089dd094a1b14a6193f6013b08
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-05 20:42:28 -0700

    nvim(lua/plugins): refactor: init.lua

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,59 +1,75 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
+  -- Download and install lazy.nvim if missing...
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath
   })
   vim.fn.system({ "git", "-C", lazypath, "checkout", "tags/stable" })
 end
-
 vim.opt.rtp:prepend(lazypath)
 
-local short_urls = require("plugins.enabled")
+
+-- Initialize plugin_names with loaded plugins.
+-- Set each plugin's lazy.nvim config function to import plugins.<plugin_name>.
+local function build_plugin_specs()
+  local plugin_names = {}
+  local plugin_specs = {}
+
+  local function build_plugin_spec(module_spec, plugin_name)
+    local plugin_spec = {}
+    for k, v in pairs(module_spec) do
+      if k ~= "_" then
+        plugin_spec[k] = v
+      end
+    end
+    table.insert(plugin_specs, plugin_spec)
+    table.insert(plugin_names, plugin_name)
+  end
+
+  local use_enabled_list = true
+
+  if use_enabled_list then
+    -- Load plugins listed in plugins/enabled.lua.
+    for _, plugin_name in ipairs(require("plugins.enabled")) do
+      local plugin_name_slug = string.gsub(plugin_name, "[-\\.]", "_")
+      local config_path = "plugins._" .. plugin_name_slug
+      local ok, module_spec = pcall(require, config_path)
+      if ok then
+        build_plugin_spec(module_spec, plugin_name)
+      end
+    end
+  else
+    -- Load all plugins in plugins/ directory automatically.
+    local files = vim.split(vim.fn.glob("*", true), "\n")
+    for _, file in ipairs(files) do
+      local basename = string.match(file, "(_.*).lua$")
+      if basename ~= nil and basename ~= "init" and basename ~= "enabled" then
+        local config_path = "plugins." .. basename
+        local ok, module_spec = pcall(require, config_path)
+        local short_url = module_spec[1]
+        local plugin_name = string.match(short_url, "^[^/]*/([^/]*)$")
+        if ok then
+          build_plugin_spec(module_spec, plugin_name)
+        end
+      end
+    end
+  end
+
+  return plugin_names, plugin_specs
+end
+
+
+local plugin_names, plugin_specs = build_plugin_specs()
+
+function _G.plugin_loaded(plugin_name)
+  return vim.tbl_contains(plugin_names, plugin_name)
+end
 
 local opts = {
   defaults = {
     -- version = "*",
   },
 }
-
-local plugin_names = {}
-
--- Initialize plugin_names with loaded plugins.
--- Set each plugin's lazy.nvim config function to import plugins.<plugin_name>.
-local function build_plugin_specs(short_urls, plugin_names)
-  local plugin_specs = {}
-
-  for i, short_url in ipairs(short_urls) do
-    local plugin_name = string.match(short_url, "([^/]*)$")
-    local plugin_name_slug = string.gsub(plugin_name, "[-\\.]", "_")
-    local config_path = "plugins._" .. plugin_name_slug
-
-    -- Indicate that plugin will be loaded.
-    table.insert(plugin_names, plugin_name)
-
-    -- Build plugin spec.
-    local plugin_spec = {} -- {short_url}
-    plugin_specs[i] = plugin_spec
-
-    local ok, module_spec = pcall(require, config_path)
-    if ok then
-      for k, v in pairs(module_spec) do
-        if k ~= "_" then
-          plugin_spec[k] = v
-        end
-      end
-    end
-  end
-
-  return plugin_specs
-end
-
-function _G.plugin_loaded(plugin_name)
-  return vim.tbl_contains(plugin_names, plugin_name)
-end
-
-local plugin_specs = build_plugin_specs(short_urls, plugin_names)
 
 require("lazy").setup(plugin_specs, opts)
