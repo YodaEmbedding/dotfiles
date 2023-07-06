02c01243c234b401f08d82d0a0c0d8f946361ff7
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-05 19:14:58 -0700

    nvim(lua/plugins): tokyonight transparent

--- nvim/.config/nvim/lua/plugins/_tokyonight_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_tokyonight_nvim.lua
@@ -5,6 +5,11 @@ return {
     -- See https://github.com/folke/tokyonight.nvim#%EF%B8%8F-configuration
     require("tokyonight").setup {
       -- style = "night",
+      transparent = true,
+      styles = {
+        -- floats = "transparent",
+        sidebars = "transparent",
+      },
     }
 
     if vim.fn.exists("+termguicolors") then
