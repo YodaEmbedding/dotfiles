7258e2b6f6eacbfab3ce1883a2f38c50ca43b2bb
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-05 20:13:15 -0700

    nvim(lua/mappings): telescope replacements for vim.lsp.buf.*
    ```lua
    map("n", "gd",              b.lsp_definitions)
    map("n", "gi",              b.lsp_implementations)
    map("n", "gr",              b.lsp_references)
    ```

--- nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
+++ nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
@@ -4,13 +4,15 @@ local map = vim.keymap.set
 
 ---@format disable-next
 function _M.on_attach()
+  -- Already handled by Telescope:
+  -- map("n", "gd",        vim.lsp.buf.definition)
+  -- map("n", "gi",        vim.lsp.buf.implementation)
+  -- map("n", "gr",        vim.lsp.buf.references)
+
   -- Navigation
   map("n", "gCi",       vim.lsp.buf.incoming_calls)
   map("n", "gCo",       vim.lsp.buf.outgoing_calls)
-  map("n", "gd",        vim.lsp.buf.definition)
   map("n", "gD",        vim.lsp.buf.declaration)
-  map("n", "g{",        vim.lsp.buf.implementation)
-  -- map("n", "gr",        vim.lsp.buf.references)
   map("n", "gS",        vim.lsp.buf.document_symbol)
   map("n", "gt",        vim.lsp.buf.type_definition)
   map("n", "gw",        vim.lsp.buf.workspace_symbol)
