fa056247d236aa83bd262d8e1c06db2b470b9642
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-07 19:45:23 -0700

    nvim(lua/plugins): dropbar.nvim [disabled until nvim 0.10]

---
+++ nvim/.config/nvim/lua/plugins/_dropbar_nvim.lua
@@ -0,0 +1,11 @@
+return {
+  "Bekaboo/dropbar.nvim",
+
+  enabled = false, -- NOTE: Requires Neovim 0.10+.0-dev or higher.
+
+  config = function()
+    require("dropbar").setup {
+
+    }
+  end
+}
