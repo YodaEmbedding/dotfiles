ade8a8381baa40b186536063e5ff2c5bf0223668
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-07 21:43:22 -0700

    nvim(lua/plugins): satellite.nvim [disabled until nvim 0.10]

---
+++ nvim/.config/nvim/lua/plugins/_satellite_nvim.lua
@@ -0,0 +1,11 @@
+return {
+  "lewis6991/satellite.nvim",
+
+  enabled = false, -- NOTE: Requires Neovim 0.10+.0-dev or higher.
+
+  config = function()
+    require("satellite").setup {
+
+    }
+  end,
+}
