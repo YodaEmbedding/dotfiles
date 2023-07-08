d5e1dd5fa4d549d6f737768b5e8833f25bf0f10d
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-07 19:44:49 -0700

    nvim(lua/plugins): marks.nvim disable builtin_marks

--- nvim/.config/nvim/lua/plugins/_marks_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_marks_nvim.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("marks").setup {
       -- default_mappings = false
-      builtin_marks = { ".", "<", ">", "^" },
+      -- builtin_marks = { ".", "<", ">", "^" },
     }
   end
 }
