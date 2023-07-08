15f7c92dac131eae94ec49c2b214816d818fa7e0
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-07 19:44:32 -0700

    nvim(lua/plugins): style

--- nvim/.config/nvim/lua/plugins/_barbecue_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_barbecue_nvim.lua
@@ -1,12 +1,14 @@
 return {
   "utilyre/barbecue.nvim",
-  -- name = "barbecue",
+
   version = "*",
+  -- name = "barbecue",
+
   dependencies = {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons", -- optional dependency
-    -- "folke/tokyonight.nvim",  -- colorscheme
   },
+
   config = function()
     require("barbecue").setup {
       kinds = require("utils.lspkind_symbols")._.kind_symbol_map,
