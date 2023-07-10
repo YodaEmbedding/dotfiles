f99de230f60df92dbc044d934bbfe21c99443d42
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-07-09 17:52:19 -0700

    nvim(lua/mappings): telescope remove smart_files

--- nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
@@ -3,22 +3,6 @@ if not _G.plugin_loaded("telescope.nvim") then
 end
 
 local b = require("telescope.builtin")
-
-local smart_files = function()
-  local in_git_repo = vim.fn.systemlist("git rev-parse --is-inside-work-tree")[1] == 'true'
-  if in_git_repo then
-    local opts = {
-      show_untracked = true,
-      use_git_root = false,
-    }
-    b.git_files(opts)
-  else
-    local opts = {}
-    b.find_files(opts)
-  end
-end
-
-
 local map = vim.keymap.set
 
 ---@format disable
