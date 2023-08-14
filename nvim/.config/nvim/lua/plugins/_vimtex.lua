7ae63d164656cbb31053c7eca4e0f1aee4b7e6a4
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-08-13 21:06:20 -0700

    nvim(lua/plugins): vimtex: don't show quickfix automatically

--- nvim/.config/nvim/lua/plugins/_vimtex.lua
+++ nvim/.config/nvim/lua/plugins/_vimtex.lua
@@ -4,6 +4,8 @@ return {
   init = function()
     vim.g.tex_flavor = "latex"
     -- vim.g.vimtex_compiler_latexmk = { build_dir = "out" }
+    -- vim.g.vimtex_quickfix_enabled = 0
     vim.g.vimtex_quickfix_method = "pplatex"
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
