0cdfbc270b0807a29c34c9445e35a7adb9dc9d42
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-10-19 00:57:09 -0700

    nvim(lua/plugins): comments

--- nvim/.config/nvim/lua/plugins/_trouble_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_trouble_nvim.lua
@@ -2,7 +2,9 @@ return {
   "folke/trouble.nvim",
 
   config = function()
-    require("trouble").setup {}
+    require("trouble").setup {
+      -- auto_preview = false,
+    }
 
     require("mappings._trouble_nvim")
   end,
--- nvim/.config/nvim/lua/plugins/_vim_python_pep8_indent.lua
+++ nvim/.config/nvim/lua/plugins/_vim_python_pep8_indent.lua
@@ -1,3 +1,9 @@
 return {
   "Vimjas/vim-python-pep8-indent",
+
+  init = function()
+    -- Defaults:
+    -- vim.g.python_pep8_indent_multiline_string = 0
+    -- vim.g.python_pep8_indent_hang_closing = 0
+  end,
 }
