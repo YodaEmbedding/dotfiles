eb0b148a85a2e1829d372145a889748a7878ccaa
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-10-29 17:55:51 -0700

    nvim(plugins): vimtex: conceal enables/disables

--- nvim/.config/nvim/lua/plugins/_vimtex.lua
+++ nvim/.config/nvim/lua/plugins/_vimtex.lua
@@ -7,6 +7,21 @@ return {
     -- vim.g.vimtex_quickfix_enabled = 0
     vim.g.vimtex_quickfix_method = "pplatex"
     vim.g.vimtex_quickfix_mode = 0
+    vim.g.vimtex_syntax_conceal = {
+      accents = 1,
+      ligatures = 1,
+      cites = 1,
+      fancy = 1,
+      spacing = 0,
+      greek = 1,
+      math_bounds = 1,
+      math_delimiters = 1,
+      math_fracs = 1,
+      math_super_sub = 1,
+      math_symbols = 1,
+      sections = 0,
+      styles = 1,
+    }
     vim.g.vimtex_view_method = "zathura"  -- also helps with synctex
   end,
 }
