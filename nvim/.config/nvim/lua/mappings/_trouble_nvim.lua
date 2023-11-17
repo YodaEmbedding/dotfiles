82ff390624c1bc3902cb9c44bf1ff659061f610c
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-11-17 15:44:09 -0800

    nvim(mappings): trouble

--- nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
@@ -3,3 +3,6 @@ if not _G.plugin_loaded("trouble.nvim") then
 end
 
 vim.keymap.set("n", "<space>t", ":TroubleToggle<CR>")
+vim.keymap.set("n", "<space>l", ":TroubleToggle loclist<CR>")
+vim.keymap.set("n", "<space>q", ":TroubleToggle quickfix<CR>")
+vim.keymap.set("n", "<space>w", ":TroubleToggle workspace_diagnostics<CR>")
