5020d6264c0a0db36c6cfe14d73f3044e48b2be0
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-12-26 02:37:48 -0800

    nvim(plugins): fix: lsp-progress autocmd

--- nvim/.config/nvim/lua/plugins/_nvim_lualine.lua
+++ nvim/.config/nvim/lua/plugins/_nvim_lualine.lua
@@ -32,8 +32,9 @@ return {
     }
 
     vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-    vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+    vim.api.nvim_create_autocmd("User", {
       group = "lualine_augroup",
+      pattern = "LspProgressStatusUpdated",
       callback = require("lualine").refresh,
     })
   end,
