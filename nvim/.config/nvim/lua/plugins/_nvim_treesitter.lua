9f43f7638a873aa9fcf36da8c323b8a2afce0774
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-05-17 14:05:56 -0700

    nvim(plugins): treesitter ignore wing

--- nvim/.config/nvim/lua/plugins/_nvim_treesitter.lua
+++ nvim/.config/nvim/lua/plugins/_nvim_treesitter.lua
@@ -12,7 +12,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup {
       ensure_installed = "all",
-      -- ignore_install = { },
+      ignore_install = { "wing" },
       highlight = {
         enable = true,
         disable = { "latex" },
