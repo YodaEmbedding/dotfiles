9bd6f10e55ff3c3602863302ae392c2f50fc577d
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-06-12 20:51:16 -0700

    nvim(mappings): TroubleToggle is no more

--- nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
@@ -2,7 +2,7 @@ if not _G.plugin_loaded("trouble.nvim") then
   do return end
 end
 
-vim.keymap.set("n", "<space>t", ":TroubleToggle<CR>")
-vim.keymap.set("n", "<space>l", ":TroubleToggle loclist<CR>")
-vim.keymap.set("n", "<space>q", ":TroubleToggle quickfix<CR>")
-vim.keymap.set("n", "<space>w", ":TroubleToggle workspace_diagnostics<CR>")
+vim.keymap.set("n", "<space>t", ":Trouble<CR>")
+vim.keymap.set("n", "<space>l", ":Trouble loclist toggle<CR>")
+vim.keymap.set("n", "<space>q", ":Trouble quickfix toggle<CR>")
+vim.keymap.set("n", "<space>w", ":Trouble diagnostics toggle<CR>")
