fe9a9a293d8f95c80f2dcddba9a471460bbdf86a
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-06-21 15:36:57 -0700

    nvim(mappings): telescope

--- nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
@@ -13,9 +13,11 @@ local b = {
   smart_open_cwd            = function() require("telescope").extensions.smart_open.smart_open({cwd_only = true, filename_first = true}) end,
   current_buffer_fuzzy_find = function() require("telescope.builtin").current_buffer_fuzzy_find() end,
   jumplist                  = function() require("telescope.builtin").jumplist() end,
+  live_grep                 = function() require("telescope.builtin").live_grep() end,
   marks                     = function() require("telescope.builtin").marks() end,
   command_history           = function() require("telescope.builtin").command_history() end,
   lsp_definitions           = function() require("telescope.builtin").lsp_definitions() end,
+  lsp_dynamic_workspace_symbols = function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
   lsp_implementations       = function() require("telescope.builtin").lsp_implementations() end,
   lsp_references            = function() require("telescope.builtin").lsp_references({show_line = false}) end,
 }
@@ -26,9 +28,11 @@ map("n", ",",               b.smart_open_cwd)
 map("n", "<Space>,",        b.smart_open)
 map("n", "-",               b.current_buffer_fuzzy_find)
 map("n", "<CR>",            b.jumplist)
+map("n", "_",               b.live_grep)
 map("n", "<Space>m",        b.marks)
 map("n", "<Space>:",        b.command_history)
 
 map("n", "gd",              b.lsp_definitions)
+map("n", "<C-t>",           b.lsp_dynamic_workspace_symbols)
 map("n", "gi",              b.lsp_implementations)
 map("n", "gr",              b.lsp_references)
