d35fe6665454c8d1c4709dda31a6f1b546d9e223
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-07-05 17:05:18 -0700

    nvim(plugins): outline.nvim

---
+++ nvim/.config/nvim/lua/plugins/_outline_nvim.lua
@@ -0,0 +1,14 @@
+return {
+  "hedyhli/outline.nvim",
+
+  lazy = true,
+
+  cmd = { "Outline", "OutlineOpen" },
+
+  keys = {
+    { "<space>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
+
+  opts = {
+  },
+}
