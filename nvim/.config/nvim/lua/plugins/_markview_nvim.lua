b75c39e3d5f5ba87b5ad52a4f8a696221520212b
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-07-05 20:50:26 -0700

    nvim(plugins): markview.nvim [disable]

---
+++ nvim/.config/nvim/lua/plugins/_markview_nvim.lua
@@ -0,0 +1,13 @@
+return {
+  "OXY2DEV/markview.nvim",
+
+  enabled = false,
+
+  dependencies = {
+    -- You may not need this if you don't lazy load
+    -- Or if the parsers are in your $RUNTIMEPATH
+    "nvim-treesitter/nvim-treesitter",
+
+    "nvim-tree/nvim-web-devicons",
+  },
+}
