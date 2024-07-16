0899ebd0e570d1408d93fd3a5eaa478f4151de07
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-07-16 15:59:44 -0700

    nvim(plugins): aerial.nvim config

--- nvim/.config/nvim/lua/plugins/_aerial_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_aerial_nvim.lua
@@ -3,6 +3,22 @@ return {
 
   config = function()
     require("aerial").setup {
+      layout = {
+        default_direction = "float",
+      },
+      -- highlight_on_hover = true,
+      autojump = true,
+      close_on_select = true,
+      float = {
+        relative = "win",
+        override = function(conf, source_winid)
+          local padding = 1
+          conf.anchor = 'NE'
+          conf.row = padding
+          conf.col = vim.api.nvim_win_get_width(source_winid) - padding
+          return conf
+        end,
+      },
     }
   end,
 }
