6f3e3bbfdbe38657fc0139f2eae1d30079f52edd
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-07-25 13:31:40 -0700

    nvim(plugins): ltex-utils.nvim dictionary

--- nvim/.config/nvim/lua/plugins/_ltex_utils_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_ltex_utils_nvim.lua
@@ -9,7 +9,27 @@ return {
 
   opts = {
     dictionary = {
-      path = vim.api.nvim_call_function("stdpath", {"state"}) .. "/ltex/",
+      -- path = vim.api.nvim_call_function("stdpath", { "state" }) .. "/ltex/",
+      -- path = vim.api.nvim_call_function("stdpath", { "config" }) .. "/spell/",
+      path = vim.fn.stdpath("config") .. "/spell/",
+
+      filename = function(lang)
+        -- For example:
+        -- lang = "en-US"
+        -- lang_prefix = "en"
+        -- encoding = "utf-8"
+        -- filename = "en.utf-8.add"
+
+        local lang_prefix = string.match(lang, "^(%a+)-")
+        local encoding = "utf-8"
+        -- NOTE: This encoding is evaluated after entering (good) and after leaving (bad) the buffer.
+        -- local encoding = vim.api.nvim_buf_get_option(0, "fileencoding")
+        local filename = lang_prefix .. "." .. encoding .. ".add"
+
+        -- print(lang, encoding, filename)
+
+        return filename
+      end,
     },
   },
 
