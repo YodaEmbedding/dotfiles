c5b291c1d55a108f72d395098085b63c37242d20
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-08-21 15:24:20 -0700

    nvim(plugins): blame.nvim move autocmd to config()

--- nvim/.config/nvim/lua/plugins/_blame_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_blame_nvim.lua
@@ -7,6 +7,31 @@ return {
       -- commit_detail_view = "current",
     }
 
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlameViewOpened",
+      callback = function(event)
+        local blame_type = event.data
+        if blame_type == "window" then
+          vim.cmd [[
+            execute "LspLensOff"
+            call feedkeys("\<C-w>h")
+          ]]
+        end
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "BlameViewClosed",
+      callback = function(event)
+        local blame_type = event.data
+        if blame_type == "window" then
+          vim.cmd [[
+            execute "LspLensOn"
+          ]]
+        end
+      end,
+    })
+
     require("mappings._blame_nvim")
   end
 }
