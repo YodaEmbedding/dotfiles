8c7194bfe420aa48b3302b84815b902121241d3b
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-08-21 20:18:59 -0700

    nvim(plugins): lazy load when possible [diffview.nvim]

--- nvim/.config/nvim/lua/plugins/_diffview_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_diffview_nvim.lua
@@ -1,6 +1,16 @@
 return {
   "sindrets/diffview.nvim",
 
+  cmd = {
+    "DiffviewClose",
+    "DiffviewFileHistory",
+    "DiffviewFocusFiles",
+    "DiffviewLog",
+    "DiffviewOpen",
+    "DiffviewRefresh",
+    "DiffviewToggleFiles",
+  },
+
   opts = {
 
   },
