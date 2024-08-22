fb69d5c7274e23a1331c1b3af483dfe9be8ee7ab
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-08-21 20:19:21 -0700

    nvim(mappings): trouble.nvim <space>t prefix

--- nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_trouble_nvim.lua
@@ -1,6 +1,6 @@
 return {
-  { "<space>t", ":Trouble<CR>", "n" },
-  { "<space>l", ":Trouble loclist toggle<CR>", "n" },
-  { "<space>q", ":Trouble quickfix toggle<CR>", "n" },
-  { "<space>w", ":Trouble diagnostics toggle<CR>", "n" },
+  { "<space>tt", ":Trouble<CR>", "n" },
+  { "<space>tl", ":Trouble loclist toggle<CR>", "n" },
+  { "<space>tq", ":Trouble quickfix toggle<CR>", "n" },
+  { "<space>td", ":Trouble diagnostics toggle<CR>", "n" },
 }
