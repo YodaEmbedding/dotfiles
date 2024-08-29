b69fe76e67785648bfb0a201167d1a2835510eec
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-08-28 18:50:16 -0700

    nvim(mappings): lsp -> telescope; remove unused <Space>z

--- nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
+++ nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
@@ -5,32 +5,31 @@ local code_action = ok and actions_preview.code_actions or vim.lsp.buf.code_acti
 
 local mappings = {
   -- Navigation
-  { "gCi",      vim.lsp.buf.incoming_calls,   desc = "Incoming calls" },
-  { "gCo",      vim.lsp.buf.outgoing_calls,   desc = "Outgoing calls" },
-  { "gD",       vim.lsp.buf.declaration,      desc = "Go to declaration" },
-  { "gS",       vim.lsp.buf.document_symbol,  desc = "Go to document symbol" },
-  { "gt",       vim.lsp.buf.type_definition,  desc = "Go to type definition" },
-  { "gw",       vim.lsp.buf.workspace_symbol, desc = "Workspace symbols" },
+  { "gD",       vim.lsp.buf.declaration,    desc = "Go to declaration" },
 
   -- Documentation
-  { "<C-k>",    vim.lsp.buf.signature_help,   "i" },
-  { "K",        vim.lsp.buf.hover,            desc = "Hover symbol" },
+  { "<C-k>",    vim.lsp.buf.signature_help, "i" },
+  { "K",        vim.lsp.buf.hover,          desc = "Hover symbol" },
 
   -- Diagnostics
-  { "<space>z", vim.diagnostic.setloclist,    desc = "Diagnostics loclist" },
-  { "<space>e", vim.diagnostic.open_float,    desc = "Diagnostics hover" },
+  { "<space>e", vim.diagnostic.open_float,  desc = "Diagnostics hover" },
 
   -- Refactoring
-  { "<space>a", code_action,                  desc = "Code actions" },
-  { "<F2>",     vim.lsp.buf.rename,           desc = "Rename symbol" },
-  { "<F4>",     vim.lsp.buf.format,           desc = "Format document" },
+  { "<space>a", code_action,                desc = "Code actions" },
+  { "<F2>",     vim.lsp.buf.rename,         desc = "Rename symbol" },
+  { "<F4>",     vim.lsp.buf.format,         desc = "Format document" },
 }
 
 if not _G.plugin_loaded("telescope.nvim") then
   local more_mappings = {
-    { "gd", vim.lsp.buf.definition,     desc = "Go to definition" },
-    { "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
-    { "gr", vim.lsp.buf.references,     desc = "Go to references" },
+    { "gd",  vim.lsp.buf.definition,       desc = "Go to definition" },
+    { "gi",  vim.lsp.buf.implementation,   desc = "Go to implementation" },
+    { "gr",  vim.lsp.buf.references,       desc = "Go to references" },
+    { "gt",  vim.lsp.buf.type_definition,  desc = "Go to type definition" },
+    { "gCi", vim.lsp.buf.incoming_calls,   desc = "Incoming calls" },
+    { "gCo", vim.lsp.buf.outgoing_calls,   desc = "Outgoing calls" },
+    { "gS",  vim.lsp.buf.document_symbol,  desc = "Document symbols" },
+    { "gw",  vim.lsp.buf.workspace_symbol, desc = "Workspace symbols" },
   }
   vim.list_extend(mappings, more_mappings)
 end
--- nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_telescope_nvim.lua
@@ -12,9 +12,14 @@ local b = {
   resume                    = function() require("telescope.builtin").resume() end,
   command_history           = function() require("telescope.builtin").command_history() end,
   lsp_definitions           = function() require("telescope.builtin").lsp_definitions() end,
-  lsp_dynamic_workspace_symbols = function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
   lsp_implementations       = function() require("telescope.builtin").lsp_implementations() end,
   lsp_references            = function() require("telescope.builtin").lsp_references({show_line = false}) end,
+  lsp_type_definitions      = function() require("telescope.builtin").lsp_type_definitions() end,
+  lsp_incoming_calls        = function() require("telescope.builtin").lsp_incoming_calls() end,
+  lsp_outgoing_calls        = function() require("telescope.builtin").lsp_outgoing_calls() end,
+  lsp_document_symbols      = function() require("telescope.builtin").lsp_document_symbols() end,
+  lsp_workspace_symbols     = function() require("telescope.builtin").lsp_workspace_symbols() end,
+  lsp_dynamic_workspace_symbols = function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
 }
 
 return {
@@ -30,7 +35,12 @@ return {
   { "<Space>:",        b.command_history,               desc = "Command history" },
 
   { "gd",              b.lsp_definitions,               desc = "Go to definition" },
-  { "<C-t>",           b.lsp_dynamic_workspace_symbols, desc = "Workspace symbols" },
-  { "gi",              b.lsp_implementations,           desc = "Implementations" },
-  { "gr",              b.lsp_references,                desc = "References" },
+  { "gi",              b.lsp_implementations,           desc = "Go to implementation" },
+  { "gr",              b.lsp_references,                desc = "Go to references" },
+  { "gt",              b.lsp_type_definitions,          desc = "Go to type definition" },
+  { "gCi",             b.lsp_incoming_calls,            desc = "Incoming calls" },
+  { "gCo",             b.lsp_outgoing_calls,            desc = "Outgoing calls" },
+  { "gS",              b.lsp_document_symbols,          desc = "Document symbols" },
+  { "gw",              b.lsp_workspace_symbols,         desc = "Workspace symbols" },
+  { "<C-t>",           b.lsp_dynamic_workspace_symbols, desc = "Workspace symbols (dynamic)" },
 }
