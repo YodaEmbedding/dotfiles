87780126971ffca80f67cc9a3299c7283f903791
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-08-28 18:23:59 -0700

    nvim(mappings): desc

--- nvim/.config/nvim/lua/mappings/_gitsigns_nvim.lua
+++ nvim/.config/nvim/lua/mappings/_gitsigns_nvim.lua
@@ -12,6 +12,7 @@ local mappings = {
         require("gitsigns").nav_hunk("next")
       end
     end,
+    desc = "Go to next Git hunk",
   },
   {
     "[c",
@@ -22,25 +23,85 @@ local mappings = {
         require("gitsigns").nav_hunk("prev")
       end
     end,
+    desc = "Go to previous Git hunk",
   },
 
   -- Actions
-  { "<leader>hs", function() require("gitsigns").stage_hunk() end },
-  { "<leader>hr", function() require("gitsigns").reset_hunk() end },
-  { "<leader>hs", function() require("gitsigns").stage_hunk { vim.fn.line("."), vim.fn.line("v") } end, "v" },
-  { "<leader>hr", function() require("gitsigns").reset_hunk { vim.fn.line("."), vim.fn.line("v") } end, "v" },
-  { "<leader>hS", function() require("gitsigns").stage_buffer() end },
-  { "<leader>hu", function() require("gitsigns").undo_stage_hunk() end },
-  { "<leader>hR", function() require("gitsigns").reset_buffer() end },
-  { "<leader>hp", function() require("gitsigns").preview_hunk() end },
-  { "<leader>hb", function() require("gitsigns").blame_line { full = true } end },
-  { "<leader>tb", function() require("gitsigns").toggle_current_line_blame() end },
-  { "<leader>hd", function() require("gitsigns").diffthis() end },
-  { "<leader>hD", function() require("gitsigns").diffthis("~") end },
-  { "<leader>td", function() require("gitsigns").toggle_deleted() end },
+  {
+    "<leader>hs",
+    function() require("gitsigns").stage_hunk() end,
+    desc = "Git stage hunk",
+  },
+  {
+    "<leader>hs",
+    function() require("gitsigns").stage_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+    "v",
+    desc = "Git stage hunk",
+  },
+  {
+    "<leader>hr",
+    function() require("gitsigns").reset_hunk() end,
+    desc = "Git reset hunk",
+  },
+  {
+    "<leader>hr",
+    function() require("gitsigns").reset_hunk { vim.fn.line("."), vim.fn.line("v") } end,
+    "v",
+    desc = "Git reset hunk",
+  },
+  {
+    "<leader>hS",
+    function() require("gitsigns").stage_buffer() end,
+    desc = "Git stage buffer",
+  },
+  {
+    "<leader>hu",
+    function() require("gitsigns").undo_stage_hunk() end,
+    desc = "Git undo stage hunk",
+  },
+  {
+    "<leader>hR",
+    function() require("gitsigns").reset_buffer() end,
+    desc = "Git reset buffer",
+  },
+  {
+    "<leader>hp",
+    function() require("gitsigns").preview_hunk() end,
+    desc = "Git preview hunk",
+  },
+  {
+    "<leader>hb",
+    function() require("gitsigns").blame_line { full = true } end,
+    desc = "Git blame line",
+  },
+  {
+    "<leader>tb",
+    function() require("gitsigns").toggle_current_line_blame() end,
+    desc = "Git blame current line toggle",
+  },
+  {
+    "<leader>hd",
+    function() require("gitsigns").diffthis() end,
+    desc = "Git diff",
+  },
+  {
+    "<leader>hD",
+    function() require("gitsigns").diffthis("~") end,
+    desc = "Git diff (HEAD~)",
+  },
+  {
+    "<leader>td",
+    function() require("gitsigns").toggle_deleted() end,
+    desc = "Git toggle deleted",
+  },
 
   -- Text object
-  { "ih",         ":<C-U>Gitsigns select_hunk<CR>",                                                     { "o", "x" } },
+  {
+    "ih",
+    ":<C-U>Gitsigns select_hunk<CR>",
+    { "o", "x" },
+    desc = "Git select hunk",
+  },
 
 }
 
--- nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
+++ nvim/.config/nvim/lua/mappings/_nvim_lsp.lua
@@ -5,40 +5,40 @@ local code_action = ok and actions_preview.code_actions or vim.lsp.buf.code_acti
 
 local mappings = {
   -- Navigation
-  { "gCi",      vim.lsp.buf.incoming_calls },
-  { "gCo",      vim.lsp.buf.outgoing_calls },
-  { "gD",       vim.lsp.buf.declaration },
-  { "gS",       vim.lsp.buf.document_symbol },
-  { "gt",       vim.lsp.buf.type_definition },
-  { "gw",       vim.lsp.buf.workspace_symbol },
+  { "gCi",      vim.lsp.buf.incoming_calls,   desc = "Incoming calls" },
+  { "gCo",      vim.lsp.buf.outgoing_calls,   desc = "Outgoing calls" },
+  { "gD",       vim.lsp.buf.declaration,      desc = "Go to declaration" },
+  { "gS",       vim.lsp.buf.document_symbol,  desc = "Go to document symbol" },
+  { "gt",       vim.lsp.buf.type_definition,  desc = "Go to type definition" },
+  { "gw",       vim.lsp.buf.workspace_symbol, desc = "Workspace symbols" },
 
   -- Documentation
-  { "<C-k>",    vim.lsp.buf.signature_help,  "i" },
-  { "K",        vim.lsp.buf.hover },
+  { "<C-k>",    vim.lsp.buf.signature_help,   "i" },
+  { "K",        vim.lsp.buf.hover,            desc = "Hover symbol" },
 
   -- Diagnostics
-  { "<space>z", vim.diagnostic.setloclist },
-  { "<space>e", vim.diagnostic.open_float },
+  { "<space>z", vim.diagnostic.setloclist,    desc = "Diagnostics loclist" },
+  { "<space>e", vim.diagnostic.open_float,    desc = "Diagnostics hover" },
 
   -- Refactoring
-  { "<space>a", code_action },
-  { "<F2>",     vim.lsp.buf.rename },
-  { "<F4>",     vim.lsp.buf.format },
+  { "<space>a", code_action,                  desc = "Code actions" },
+  { "<F2>",     vim.lsp.buf.rename,           desc = "Rename symbol" },
+  { "<F4>",     vim.lsp.buf.format,           desc = "Format document" },
 }
 
 if not _G.plugin_loaded("telescope.nvim") then
   local more_mappings = {
-    { "gd", vim.lsp.buf.definition },
-    { "gi", vim.lsp.buf.implementation },
-    { "gr", vim.lsp.buf.references },
+    { "gd", vim.lsp.buf.definition,     desc = "Go to definition" },
+    { "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
+    { "gr", vim.lsp.buf.references,     desc = "Go to references" },
   }
   vim.list_extend(mappings, more_mappings)
 end
 
 if not _G.plugin_loaded("delimited.nvim") then
   local more_mappings = {
-    { "[d", vim.diagnostic.goto_prev },
-    { "]d", vim.diagnostic.goto_next },
+    { "[d", vim.diagnostic.goto_prev, desc = "Go to previous diagnostic" },
+    { "]d", vim.diagnostic.goto_next, desc = "Go to next diagnostic" },
   }
   vim.list_extend(mappings, more_mappings)
 end
