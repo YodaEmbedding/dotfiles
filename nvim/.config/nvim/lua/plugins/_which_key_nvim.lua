81b3f063a7946d093457ee18154d512f8827e0d6
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-09-05 11:42:08 -0700

    nvim(plugins): which-key.nvim groups

--- nvim/.config/nvim/lua/plugins/_which_key_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_which_key_nvim.lua
@@ -4,6 +4,28 @@ return {
   event = "VeryLazy",
 
   opts = {
+    spec = {
+      {
+        "<space>g",
+        group = true,
+        desc = "Git",
+      },
+      {
+        "<space>t",
+        group = true,
+        desc = "Trouble",
+      },
+      {
+        "<leader>h",
+        group = true,
+        desc = "Gitsigns hunk",
+      },
+      {
+        "<leader>t",
+        group = true,
+        desc = "Gitsigns toggle",
+      },
+    },
   },
 
   keys = require("mappings._which_key_nvim"),
