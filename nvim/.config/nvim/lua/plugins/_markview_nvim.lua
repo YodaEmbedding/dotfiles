f75b30ad1d4ce90a465473f6f107fcb66bc1834a
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-01-09 17:30:00 -0800

    nvim(plugins): render-markdown.nvim

--- nvim/.config/nvim/lua/plugins/_markview_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_markview_nvim.lua
@@ -1,6 +1,8 @@
 return {
   "OXY2DEV/markview.nvim",
 
+  enabled = false,
+
   dependencies = {
     -- You may not need this if you don't lazy load
     -- Or if the parsers are in your $RUNTIMEPATH
---
+++ nvim/.config/nvim/lua/plugins/_render_markdown_nvim.lua
@@ -0,0 +1,17 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+
+    "nvim-tree/nvim-web-devicons",
+    -- "echasnovski/mini.nvim",
+    -- "echasnovski/mini.icons",
+  },
+
+  ---@module "render-markdown"
+  ---@type render.md.UserConfig
+  opts = {
+
+  },
+}
