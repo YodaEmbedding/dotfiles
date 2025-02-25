4940caa05932b184a98feba6eb40f9170070957f
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-02-24 17:36:44 -0800

    nvim(plugins): local-highlight: deps snacks

--- nvim/.config/nvim/lua/plugins/_local_highlight_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_local_highlight_nvim.lua
@@ -1,6 +1,11 @@
 return {
   "tzachar/local-highlight.nvim",
 
+  dependencies = {
+    "folke/snacks.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+
   opts = {
     disable_file_types = { "tex", "markdown" },
     hlgroup = "LocalHighlight",
