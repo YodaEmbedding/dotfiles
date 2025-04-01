9dd2251ac9c27aa6ce3410212586e067797a8d20
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-03-31 20:56:53 -0700

    nvim(plugins): delimited.nvim [enable]

--- nvim/.config/nvim/lua/plugins/_delimited_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_delimited_nvim.lua
@@ -1,8 +1,29 @@
 return {
   "mizlan/delimited.nvim",
 
-  enabled = false,
-
   opts = {
+  },
+
+  keys = {
+    {
+      "[d",
+      function() require("delimited").goto_prev {} end,
+      desc = "Go to previous diagnostic",
+    },
+    {
+      "]d",
+      function() require("delimited").goto_next {} end,
+      desc = "Go to next diagnostic",
+    },
+    {
+      "[D",
+      function() require("delimited").goto_prev { severity = vim.diagnostic.severity.ERROR } end,
+      desc = "Go to previous diagnostic",
+    },
+    {
+      "]D",
+      function() require("delimited").goto_next { severity = vim.diagnostic.severity.ERROR } end,
+      desc = "Go to next diagnostic",
+    },
   },
 }
