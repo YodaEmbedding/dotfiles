d921045e222d983e39d02d2a5da349fefc131ce9
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-03-31 21:08:58 -0700

    nvim(mappings): inline into plugins.*.keys [continued]

--- nvim/.config/nvim/lua/plugins/disabled/_tmux_nvim.lua
+++ nvim/.config/nvim/lua/plugins/disabled/_tmux_nvim.lua
@@ -3,7 +3,19 @@ return {
 
   enabled = false,
 
-  keys = require("mappings._tmux_nvim"),
+  keys = {
+    -- NOTE: all_modes = { "n", "i", "c", "v", "o", "t", "l" }
+
+    { "<M-h>", function() require("tmux").move_left() end,     { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-j>", function() require("tmux").move_bottom() end,   { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-k>", function() require("tmux").move_top() end,      { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-l>", function() require("tmux").move_right() end,    { "n", "i", "c", "v", "o", "t", "l" } },
+
+    { "<M-H>", function() require("tmux").resize_left() end,   { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-J>", function() require("tmux").resize_bottom() end, { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-K>", function() require("tmux").resize_top() end,    { "n", "i", "c", "v", "o", "t", "l" } },
+    { "<M-L>", function() require("tmux").resize_right() end,  { "n", "i", "c", "v", "o", "t", "l" } },
+  },
 
   config = function()
     require("tmux").setup {
