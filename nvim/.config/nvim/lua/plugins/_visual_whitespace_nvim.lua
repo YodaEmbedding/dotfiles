d6057d2dacb83fb7dccf5238432224532152efb7
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-03-31 20:32:52 -0700

    nvim(plugins): visual-whitespace.nvim: VisualNonText.bg = Visual.bg

--- nvim/.config/nvim/lua/plugins/_visual_whitespace_nvim.lua
+++ nvim/.config/nvim/lua/plugins/_visual_whitespace_nvim.lua
@@ -6,4 +6,25 @@ return {
   --     link = "Visual",
   --   },
   -- },
+
+  config = function(_, opts)
+    require("visual-whitespace").setup(opts)
+
+    local default_fg = "#5d5f71"
+
+    local on_colorscheme_change = function()
+      local visual_hl = vim.api.nvim_get_hl(0, { name = "Visual" })
+      vim.api.nvim_set_hl(0, "VisualNonText", {
+        fg = default_fg,
+        bg = visual_hl and visual_hl.bg,
+      })
+    end
+
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "*",
+      callback = on_colorscheme_change,
+    })
+
+    on_colorscheme_change()
+  end,
 }
