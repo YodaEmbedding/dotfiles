95605e27b557e7ca089fbc3c2231010f642536f2
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-04-01 19:06:26 -0700

    nvim(options): convert to table

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,27 +1,85 @@
-local g = vim.g
-local o = vim.opt
+---@format disable-next
+local options = {
 
----@format disable
+  -- Completion
+  -- completeopt = "menuone,noinsert,noselect",
 
--- Colors
+  -- Indenting, tabs, spacing
+  expandtab = true,                   -- Spaces for indent
+  joinspaces = false,                 -- Join sentences with single space
+  shiftround = true,                  -- Round >> to nearest shiftwidth multiple
+  shiftwidth = 4,                     -- Spaces for >>
+  softtabstop = 4,                    -- Spaces for tab
+  tabstop = 4,                        -- Tab display width
+
+  -- Miscellaneous
+  hidden = true,                      -- Allow hiding unsaved buffers
+  mouse = "a",                        -- Mouse interactivity
+  title = true,                       -- Set terminal title
+  -- titlestring = "%t",              -- Set current file as terminal title
+  wildmenu = true,                    -- Enhanced command-line completion
+  wildmode = "longest,list,full",     -- Command-line completion behavior
+  updatetime = 500,                   -- ms until CursorHold fires and swap save
+
+  -- Search
+  hlsearch = true,                    -- Highlight search results
+  ignorecase = true,                  -- Ignore case
+  inccommand = "split",               -- Incremental highlight during :%s
+  smartcase = true,                   -- Ignore case if lower
+
+  -- Undo
+  undofile = true,                    -- Undo history
+
+  -- Visual
+  colorcolumn = "80",                 -- Column guide
+  cursorline = true,                  -- Highlight current line
+  laststatus = 2,                     -- Status line
+  linebreak = true,                   -- Wrap on word breaks
+  numberwidth = 2,                    -- Numbering column width
+  relativenumber = true,              -- Relative line numbers
+  scrolloff = 2,                      -- Always display lines around cursor
+  signcolumn = "auto:1-3",            -- Sign column
+
+  statusline = table.concat({
+    " ",                              -- -
+    -- "%t",                          -- Tail of the filename
+    -- " ",                           -- -
+    -- "∈",                           -- ∈
+    -- " ",                           -- -
+    -- "%{expand('%:~:.:h')}",        -- Parents till cwd
+    "%{expand('%:~:.')}",             -- Relative filepath
+    "%h",                             -- Help file flag
+    "%r",                             -- Read only flag
+    "%m",                             -- Modified flag
+    "%=",                             -- Left/right separator
+    " ",                              -- -
+    "  ",                             -- --
+    "%{&filetype}",                   -- Filetype
+    " ",                              -- -
+    "%{&ff}",                         -- File format
+    " ",                              -- -
+    "%{&expandtab?\"sp\":\"tab\"}-",  -- Indent settings
+    "%{&shiftwidth}",                 -- Indent settings
+    "  ",                             -- --
+    "%2v",                            -- Cursor column (virtual)
+    "  ",                             -- --
+    "(%l / %L)",                      -- Cursor line/total lines
+    " ",                              -- -
+  }),
+
+}
+
+---@format disable-next
 if vim.fn.exists("+termguicolors") then
-  o.termguicolors = true              -- True color (24-bit)
+  options.termguicolors = true        -- True color (24-bit)
 end
 
--- Completion
--- o.completeopt = "menuone,noinsert,noselect"
--- vim.o.shortmess = vim.o.shortmess .. "c"
-
--- Indenting, tabs, spacing
-o.expandtab = true                    -- Spaces for indent
-o.joinspaces = false                  -- Join sentences with single space
-o.shiftround = true                   -- Round >> to nearest shiftwidth multiple
-o.shiftwidth = 4                      -- Spaces for >>
-o.softtabstop = 4                     -- Spaces for tab
-o.tabstop = 4                         -- Tab display width
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
 -- Indenting (python)
-g.python_indent = {
+vim.g.python_indent = {
   continue = "shiftwidth()",
   nested_paren = "shiftwidth()",
   open_paren = "shiftwidth()",
@@ -29,59 +87,3 @@ g.python_indent = {
   -- close_paren = "0",
   closed_paren_align_last_line = false,
 }
-
--- Miscellaneous
-o.hidden = true                       -- Allow hiding unsaved buffers
-o.mouse = "a"                         -- Mouse interactivity
-o.title = true                        -- Set terminal title
--- o.titlestring = "%t"                  -- Set current file as terminal title
-o.wildmenu = true                     -- Enhanced command-line completion
-o.wildmode = "longest,list,full"      -- Command-line completion behavior
-o.updatetime = 500                    -- ms until CursorHold fires and swap save
-
--- Search
-o.hlsearch = true                     -- Highlight search results
-o.ignorecase = true                   -- Ignore case
-o.inccommand = "split"                -- Incremental highlight during :%s
-o.smartcase = true                    -- Ignore case if lower
-
--- Undo
-o.undofile = true                     -- Undo history
-
--- Visual
-o.colorcolumn = "80"                  -- Column guide
-o.cursorline = true                   -- Highlight current line
-o.laststatus = 2                      -- Status line
-o.linebreak = true                    -- Wrap on word breaks
-o.numberwidth = 2                     -- Numbering column width
-o.relativenumber = true               -- Relative line numbers
-o.scrolloff = 2                       -- Always display lines around cursor
-o.signcolumn = "auto:1-3"             -- Sign column
-
--- Statusline
-o.statusline = table.concat({
-  " ",                              -- -
-  -- "%t",                          -- Tail of the filename
-  -- " ",                           -- -
-  -- "∈",                           -- ∈
-  -- " ",                           -- -
-  -- "%{expand('%:~:.:h')}",        -- Parents till cwd
-  "%{expand('%:~:.')}",             -- Relative filepath
-  "%h",                             -- Help file flag
-  "%r",                             -- Read only flag
-  "%m",                             -- Modified flag
-  "%=",                             -- Left/right separator
-  " ",                              -- -
-  "  ",                             -- --
-  "%{&filetype}",                   -- Filetype
-  " ",                              -- -
-  "%{&ff}",                         -- File format
-  " ",                              -- -
-  "%{&expandtab?\"sp\":\"tab\"}-",  -- Indent settings
-  "%{&shiftwidth}",                 -- Indent settings
-  "  ",                             -- --
-  "%2v",                            -- Cursor column (virtual)
-  "  ",                             -- --
-  "(%l / %L)",                      -- Cursor line/total lines
-  " ",                              -- -
-})
