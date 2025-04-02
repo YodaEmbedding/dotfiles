fa90c49417368a6ac383fc53572ba62da72ed9f5
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-04-01 20:46:23 -0700

    nvim(autocmds): auto-create subdirs

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,6 +9,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   end,
 })
 
+-- Auto create directory when saving a file (mkdir -p)
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
