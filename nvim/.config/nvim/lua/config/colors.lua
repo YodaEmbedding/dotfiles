9a6a9546d0751a632020138c20fc8fedc618126c
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-04-01 19:52:45 -0700

    nvim(colors): to k,v table

--- nvim/.config/nvim/lua/config/colors.lua
+++ nvim/.config/nvim/lua/config/colors.lua
@@ -1,54 +1,54 @@
 local NONE = nil
 
-local colors = {
+local highlights = {
 
   -- vim diff
-  { "DiffAdd",                     { bg = "#313e52", fg = NONE, }, },
-  { "DiffChange",                  { bg = "#43365c", fg = NONE, }, },
-  { "DiffDelete",                  { bg = "#663b63", fg = NONE, }, },
-  { "DiffText",                    { bg = "#554575", fg = NONE, }, },
+  DiffAdd                     = { bg = "#313e52", fg = NONE, },
+  DiffChange                  = { bg = "#43365c", fg = NONE, },
+  DiffDelete                  = { bg = "#663b63", fg = NONE, },
+  DiffText                    = { bg = "#554575", fg = NONE, },
 
   -- Diagnostics
-  { "DiagnosticVirtualTextError",  { bg = NONE, fg = "#905566", }, }, -- Default: bg = "#362c3d", fg = "#db4b4b",
-  { "DiagnosticVirtualTextWarn",   { bg = NONE, fg = "#887755", }, }, -- Default: bg = "#373640", fg = "#e0af68",
-  { "DiagnosticVirtualTextInfo",   { bg = NONE, fg = "#557788", }, }, -- Default: bg = "#22374b", fg = "#0db9d7",
-  { "DiagnosticVirtualTextHint",   { bg = NONE, fg = "#665577", }, }, -- Default: bg = "#233745", fg = "#1abc9c",
+  DiagnosticVirtualTextError  = { bg = NONE, fg = "#905566", }, -- Default: bg = "#362c3d", fg = "#db4b4b",
+  DiagnosticVirtualTextWarn   = { bg = NONE, fg = "#887755", }, -- Default: bg = "#373640", fg = "#e0af68",
+  DiagnosticVirtualTextInfo   = { bg = NONE, fg = "#557788", }, -- Default: bg = "#22374b", fg = "#0db9d7",
+  DiagnosticVirtualTextHint   = { bg = NONE, fg = "#665577", }, -- Default: bg = "#233745", fg = "#1abc9c",
 
   -- gitsigns.nvim
-  { "GitSignsAdd",                 { fg = "#2cc474", }, },
-  { "GitSignsChange",              { fg = "#8e73f6", }, },
-  { "GitSignsDelete",              { fg = "#ff436c", }, },
+  GitSignsAdd                 = { fg = "#2cc474", },
+  GitSignsChange              = { fg = "#8e73f6", },
+  GitSignsDelete              = { fg = "#ff436c", },
 
   -- local-highlight.nvim
-  { "LocalHighlight",              { bg = "#513965", }, }, -- Default: fg = "#dcd7ba",
-  { "CWLocalHighlight",            { bg = "#443055", }, },
+  LocalHighlight              = { bg = "#513965", }, -- Default: fg = "#dcd7ba",
+  CWLocalHighlight            = { bg = "#443055", },
 
   -- indent-blankline.nvim
-  { "IndentBlanklineChar",         { fg = "#382935", nocombine = true }, }, -- v2
-  { "IblScope",                    { fg = "#382935", nocombine = true }, }, -- v3
+  IndentBlanklineChar         = { fg = "#382935", nocombine = true }, -- v2
+  IblScope                    = { fg = "#382935", nocombine = true }, -- v3
 
   -- match-visual.nvim
-  { "VisualMatch",                 { bg = "#514d6e", }, },
+  VisualMatch                 = { bg = "#514d6e", },
 
   -- nvim-cmp
-  { "CmpItemAbbr",                 { fg = "#bbbbbb", }, },
-  { "CmpItemAbbrMatch",            { fg = "#ccccff", }, },
-  { "CmpItemAbbrMatchFuzzy",       { fg = "#aaaaff", }, },
-  { "CmpItemKind",                 { fg = "#bbbbee", }, },
-  { "CmpItemMenu",                 { fg = "#bbbbee", }, },
+  CmpItemAbbr                 = { fg = "#bbbbbb", },
+  CmpItemAbbrMatch            = { fg = "#ccccff", },
+  CmpItemAbbrMatchFuzzy       = { fg = "#aaaaff", },
+  CmpItemKind                 = { fg = "#bbbbee", },
+  CmpItemMenu                 = { fg = "#bbbbee", },
 
   -- nvim-hlslens
-  { "HlSearchLens",                { bg = "#362956", fg = "#554466", }, },
+  HlSearchLens                = { bg = "#362956", fg = "#554466", },
 
   -- nvim-treesitter-context
-  { "TreesitterContext",           { bg = "#403659", }, },
-  { "TreesitterContextLineNumber", { bg = NONE, fg = "#554466", }, },
+  TreesitterContext           = { bg = "#403659", },
+  TreesitterContextLineNumber = { bg = NONE, fg = "#554466", },
 
   -- virt-column.nvim
-  { "VirtColumn",                  { fg = "#402d3e", }, },
+  VirtColumn                  = { fg = "#402d3e", },
 
 }
 
-for _, c in ipairs(colors) do
-  vim.api.nvim_set_hl(0, c[1], c[2])
+for key, attrs in pairs(highlights) do
+  vim.api.nvim_set_hl(0, key, attrs)
 end
