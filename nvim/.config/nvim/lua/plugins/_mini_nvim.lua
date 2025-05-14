b38f2c8dcb94891bf55b29dec3d0d86beb6cacdf
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-05-13 18:19:44 -0700

    nvim(plugins): mini.nvim

---
+++ nvim/.config/nvim/lua/plugins/_mini_nvim.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.nvim",
+
+  config = function()
+    require("mini.comment").setup {
+      -- options = {
+      -- },
+    }
+
+    -- require("mini.pairs").setup {
+    --   -- options = {
+    --   -- },
+    -- }
+  end,
+}
