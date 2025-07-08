a76de3769196e69d34d3c57451961b5efa72bf5c
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-07-08 14:36:05 -0700

    aliases: alias c="bat --pager='less -RFi'"

--- aliases/.alias/aliases.sh
+++ aliases/.alias/aliases.sh
@@ -286,7 +286,7 @@ alias atqq='for j in $(atq | sort -k6,6 -k3,3M -k4,4 -k5,5 | cut -f 1); do atq |
 alias atqqq='for j in $(atq | sort -k6,6 -k3,3M -k4,4 -k5,5 | cut -f 1); do atq | grep -P "^$j\t"; at -c "$j" | tail -n 2; done'
 
 # bat
-alias c='bat'
+alias c="bat --pager='less -RFi'"
 
 batw() {
   # bat with word wrapping.
