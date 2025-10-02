13e1d0d145757bba1f6d3d2437eccd006421b1d1
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-10-01 21:01:03 -0700

    nvim(plugins): lspconfig: lua/plugins/lspconfig/ loader no longer needed (since lsp/)

--- nvim/.config/nvim/lua/plugins/_nvim_lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/_nvim_lspconfig.lua
@@ -42,11 +42,9 @@ return {
       -- "rnix",
     },
 
-    setup_servers = function(servers, configs, config_defaults)
+    setup_servers = function(servers, config_defaults)
+      vim.lsp.config("*", config_defaults)
       for _, lsp in ipairs(servers) do
-        local config = configs[lsp] or {}
-        config = vim.tbl_extend("keep", config, config_defaults)
-        vim.lsp.config(lsp, config)
         vim.lsp.enable(lsp)
       end
     end,
@@ -68,31 +66,10 @@ return {
       end
     end,
 
-    -- Load all server configs in plugins/lspconfig/ directory automatically.
-    load_configs = function()
-      local configs = {}
-      local curr_script_dir = vim.fs.dirname(debug.getinfo(2, "S").source:sub(2))
-      local files = vim.split(vim.fn.glob(curr_script_dir .. "/lspconfig/*", true), "\n")
-      for _, file in ipairs(files) do
-        local basename = string.match(vim.fs.basename(file), "(.*).lua$")
-        if basename ~= nil and basename ~= "init" then
-          local config_path = "plugins.lspconfig." .. basename
-          local module_config = require("utils").try_require(config_path)
-          if module_config then
-            configs[basename] = module_config
-          end
-        end
-      end
-      return configs
-    end,
-
   },
 
   config = function()
     local this = require("plugins._nvim_lspconfig")._
-
-    -- server-specific configs
-    local configs = this.load_configs()
 
     -- config that is common across servers
     local config_defaults = {
@@ -107,6 +84,6 @@ return {
 
     require("plugins._nvim_cmp")._.on_lspconfig_load()
 
-    this.setup_servers(this.servers, configs, config_defaults)
+    this.setup_servers(this.servers, config_defaults)
   end,
 }
