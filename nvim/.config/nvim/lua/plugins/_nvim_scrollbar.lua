7fe2c4929dcecb9459ad83411e1a46ad55f4efd9
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-10-01 19:57:37 -0700

    nvim(plugins): nvim-scrollbar: marksmarks

--- nvim/.config/nvim/lua/plugins/_nvim_scrollbar.lua
+++ nvim/.config/nvim/lua/plugins/_nvim_scrollbar.lua
@@ -16,5 +16,24 @@ return {
   config = function(_, opts)
     require("scrollbar").setup(opts)
     require("scrollbar.handlers.gitsigns").setup()
+
+    require("scrollbar.handlers").register("marksmarks", function(bufnr)
+      local excluded_marks = ""  -- e.g., "z"
+      local marks = vim.fn.getmarklist(bufnr) ---@diagnostic disable-line: param-type-mismatch
+      local out = {}
+      for _, markObj in pairs(marks) do
+        local mark = markObj.mark:sub(2, 2)
+        local isLetter = mark:lower() ~= mark:upper()
+        if isLetter and not (excluded_marks:find(mark)) then
+          table.insert(out, {
+            line = markObj.pos[2],
+            text = mark,
+            type = "Info",
+            level = 0,
+          })
+        end
+      end
+      return out
+    end)
   end,
 }
