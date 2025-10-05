e52d35d4a4a98d31cd857bda05ff4208d5b56b7b
Mateen Ulhaq <mulhaq2005@gmail.com>
2025-10-04 20:23:15 -0700

    nvim(plugins): snacks

---
+++ nvim/.config/nvim/lua/plugins/_snacks_nvim.lua
@@ -0,0 +1,25 @@
+return {
+  "folke/snacks.nvim",
+
+  priority = 1000,
+  lazy = false,
+
+  keys = {
+  },
+
+  ---@type snacks.Config
+  opts = {
+    -- bigfile = { enabled = true },
+    -- dashboard = { enabled = true },
+    -- explorer = { enabled = true },
+    -- indent = { enabled = true },
+    -- input = { enabled = true },
+    -- picker = { enabled = true },
+    -- notifier = { enabled = true },
+    -- quickfile = { enabled = true },
+    -- scope = { enabled = true },
+    -- scroll = { enabled = true },
+    -- statuscolumn = { enabled = true },
+    -- words = { enabled = true },
+  },
+}
