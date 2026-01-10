a3a95f8963413697533b2f1410b0f3637fc9d73a
Mateen Ulhaq <mulhaq2005@gmail.com>
2026-01-09 18:12:51 -0800

    nvim(plugins): teide.nvim [disabled]

---
+++ nvim/.config/nvim/lua/plugins/disabled/_teide_nvim.lua
@@ -0,0 +1,13 @@
+return {
+  "serhez/teide.nvim",
+
+  enabled = false,
+
+  lazy = false,
+  priority = 1000,
+
+  -- See https://github.com/serhez/teide.nvim#%EF%B8%8F-configuration
+  opts = {
+    transparent = true,
+  },
+}
